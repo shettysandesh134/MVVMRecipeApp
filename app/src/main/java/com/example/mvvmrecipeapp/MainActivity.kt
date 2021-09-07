a667238b809658c7cd2cdf89776c5c27f91ec13b
sandeshshetty <sandesh.shetty134@gmail.com>
2021-09-08 01:37:55 +1000

    dependencies added

--- app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.mvvmrecipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 
+//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
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        setContent{
+            Column(
+                Modifier.padding(16.dp)
+            ) {
+                Text("Hey look some text")
+                Spacer(Modifier.padding(top=10.dp))
+                Button(
+                    onClick = {}
+                ){
+                    Text(text = "A Button")
+                }
+            }
+        }
+//
+
+
     }
+
 }
