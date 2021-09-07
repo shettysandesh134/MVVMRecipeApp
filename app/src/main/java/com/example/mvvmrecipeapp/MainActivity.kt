e8646992da32dc4bf000c71bc4e54112dd40c923
sandeshshetty <sandesh.shetty134@gmail.com>
2021-09-08 03:23:16 +1000

    Column layout with vertical scroll and textStyle

--- app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
@@ -3,38 +3,63 @@ package com.example.mvvmrecipeapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 //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
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
 
-        setContent{
+        setContent {
+
             Column(
-                Modifier.padding(16.dp)
+                modifier = Modifier.fillMaxSize()
+                    .verticalScroll(rememberScrollState())
+                    .background(color = Color(0xfff2f2f2)),
             ) {
-                Text("Hey look some text")
-                Spacer(Modifier.padding(top=10.dp))
-                Button(
-                    onClick = {}
-                ){
-                    Text(text = "A Button")
+                Image(
+                    painter = painterResource(id = R.drawable.happy_meal_small),
+                    contentDescription = "",
+                    modifier = Modifier.height(300.dp),
+                    contentScale = ContentScale.Crop
+                )
+                Column(
+                    Modifier.padding(16.dp)
+                ) {
+                     Text(
+                         text = "Happy Meal",
+                         fontSize = 26.sp
+                     )
+                     Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(
+                        text = "800 Calories",
+                        fontSize = 17.sp)
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(
+                        text = "$5.99",
+                        color = Color.Green,
+                        fontSize = 17.sp
+                    )
                 }
             }
-        }
-//
 
+        }
 
     }
+    
 
 }
