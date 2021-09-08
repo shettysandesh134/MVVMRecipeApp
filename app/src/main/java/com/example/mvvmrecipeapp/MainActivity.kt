f2b4fcab83090f5587c8a19c63cb930ff699cf8b
sandeshshetty <sandesh.shetty134@gmail.com>
2021-09-09 00:28:27 +1000

    Fragment and how to add customView using jetpackcompose

--- app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/com/example/mvvmrecipeapp/MainActivity.kt
@@ -22,88 +22,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-//        setContent {
-//            Column {
-//                Column(
-//                    modifier = Modifier
-//                        .fillMaxWidth()
-//                        .height(200.dp)
-//                        .border(border = BorderStroke(width = 1.dp, color = Color.Black)),
-//                    verticalArrangement = Arrangement.SpaceEvenly
-//                ) {
-//                    Text(
-//                        text = "ITEM1",
-//                        modifier = Modifier.align(Alignment.CenterHorizontally)
-//                    )
-//                    Text(
-//                        text = "ITEM2",
-//                        modifier = Modifier.align(Alignment.CenterHorizontally)
-//                    )
-//                }
-//                Row(
-//                    modifier = Modifier.width(200.dp)
-//                        .height(200.dp)
-//                        .border(border = BorderStroke(width = 1.dp, color = Color.Black)),
-//                    horizontalArrangement = Arrangement.Center
-//                ) {
-//                    Text(
-//                        text = "ITEM2",
-//                        modifier = Modifier.align(Alignment.CenterVertically)
-//                    )
-//                }
-//            }
-//        }
-
-
-        setContent {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .verticalScroll(rememberScrollState())
-                    .background(color = Color(0xfff2f2f2)),
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.happy_meal_small),
-                    contentDescription = "",
-                    modifier = Modifier.height(300.dp),
-                    contentScale = ContentScale.Crop
-                )
-                Column(
-                    Modifier.padding(16.dp)
-                ) {
-                    Row (
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ){
-                        Text(
-                            text = "Happy Meal",
-                            fontSize = 26.sp
-                        )
-                        Text(
-                            text = "$5.99",
-                            color = Color(0xFF85bb65),
-                            fontSize = 17.sp,
-                            modifier = Modifier.align(Alignment.CenterVertically)
-                        )
-                    }
-                     Spacer(modifier = Modifier.padding(top = 10.dp))
-                    Text(
-                        text = "800 Calories",
-                        fontSize = 17.sp)
-                    Spacer(modifier = Modifier.padding(top = 10.dp))
-                    Button(
-                            onClick = { /*TODO*/ },
-                            modifier = Modifier.align(Alignment.CenterHorizontally)
-                        ) {
-                            Text(text = "ORDER NOW")
-                        }
-
-
-                }
-            }
-
-        }
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, RecipeListFragment())
+            .commit()
 
     }
     
