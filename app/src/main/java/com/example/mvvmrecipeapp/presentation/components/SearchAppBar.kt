2542c6204707e595466143985dadb03ad5981395
sandeshshetty <sandesh.shetty134@gmail.com>
2021-09-29 00:00:07 +1000

    State hoisting implemented

---
+++ app/src/main/java/com/example/mvvmrecipeapp/presentation/components/SearchAppBar.kt
@@ -0,0 +1,110 @@
+package com.example.mvvmrecipeapp.presentation.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.example.mvvmrecipeapp.presentation.ui.recipe_list.FoodCategory
+import com.example.mvvmrecipeapp.presentation.ui.recipe_list.getAllFoodCategory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@Composable
+fun SearchAppBar(
+    query: String,
+    onQueryChanged: (String) -> Unit,
+    onExecuteSearch: () -> Unit,
+    focusManager: FocusManager,
+    scrollState: LazyListState,
+    scope: CoroutineScope,
+    itemIndex: Int,
+    scrollOffset: Int,
+    selectedCategory: FoodCategory?,
+    onSelectedCategoryChanged: (String) -> Unit,
+    onChangeHorizontalScrollPosition: (LazyListState) -> Unit
+) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth(),
+        color = Color.White,
+        elevation = 8.dp,
+    ) {
+        Column {
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                TextField(
+                    modifier = Modifier
+                        .fillMaxWidth(0.9f)
+                        .padding(8.dp),
+                    value = query,
+                    onValueChange = { newValue->
+                        onQueryChanged(newValue)
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Search
+                    ),
+                    label = {
+                        Text(text = "Search")
+                    },
+                    leadingIcon = {
+                        Icon(Icons.Filled.Search, "")
+                    },
+                    keyboardActions = KeyboardActions(onSearch = {
+                        onExecuteSearch()
+                        focusManager?.clearFocus()
+                    }),
+                    textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
+                    colors = TextFieldDefaults.textFieldColors(backgroundColor = MaterialTheme.colors.surface)
+
+                )
+                // end of TextField
+            }
+
+            LazyRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 8.dp, bottom = 8.dp),
+                state = scrollState
+            ){
+                scope.launch {
+                    scrollState.scrollToItem(itemIndex, scrollOffset)
+                }
+
+                itemsIndexed(
+                    items = getAllFoodCategory()
+                ){ index, category ->
+                    FoodCategoryChip(
+                        category = category.value,
+                        isSelected = selectedCategory == category,
+                        onSelectedCategoryChanged = {
+                            onSelectedCategoryChanged(it)
+                            onChangeHorizontalScrollPosition(scrollState)
+                        },
+                        onExecuteSearch = { onExecuteSearch() }
+                    )
+                }
+            }
+        }
+
+    }
+}
--- app/src/main/java/com/example/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/example/mvvmrecipeapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -1,43 +1,38 @@
 package com.example.mvvmrecipeapp.presentation.ui.recipe_list
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.viewModelScope
-import androidx.navigation.findNavController
-import com.example.mvvmrecipeapp.R
 import com.example.mvvmrecipeapp.presentation.components.FoodCategoryChip
 import com.example.mvvmrecipeapp.presentation.components.RecipeCard
-import com.example.mvvmrecipeapp.util.TAG
+import com.example.mvvmrecipeapp.presentation.components.SearchAppBar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -79,74 +74,20 @@ class RecipeListFragment: Fragment() {
 
                 Column {
 
-                    Surface(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        color = Color.White,
-                        elevation = 8.dp,
-                    ) {
-                        Column {
+                    SearchAppBar(
+                        query = query,
+                        onQueryChanged = viewModel::onQueryChanged,
+                        onExecuteSearch = viewModel::newSearch,
+                        focusManager = focusManager,
+                        scrollState = scrollState,
+                        scope = scope,
+                        itemIndex = scrollState.firstVisibleItemIndex,
+                        scrollOffset = scrollState.firstVisibleItemScrollOffset,
+                        selectedCategory = selectedCategory,
+                        onSelectedCategoryChanged = viewModel::onSelectedCategoryChanged,
+                        onChangeHorizontalScrollPosition = viewModel :: onChangeHorizontalScrollPosition
+                    )
 
-                            Row(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                            ) {
-                                TextField(
-                                    modifier = Modifier
-                                        .fillMaxWidth(0.9f)
-                                        .padding(8.dp),
-                                    value = query,
-                                    onValueChange = { newValue->
-                                        viewModel.onQueryChanged(newValue)
-                                    },
-                                    keyboardOptions = KeyboardOptions(
-                                        keyboardType = KeyboardType.Text,
-                                        imeAction = ImeAction.Search
-                                    ),
-                                    label = {
-                                        Text(text = "Search")
-                                    },
-                                    leadingIcon = {
-                                        Icon(Icons.Filled.Search, "")
-                                    },
-                                    keyboardActions = KeyboardActions(onSearch = {
-                                        viewModel.newSearch()
-                                        focusManager?.clearFocus()
-                                    }),
-                                    textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
-                                    colors = TextFieldDefaults.textFieldColors(backgroundColor = MaterialTheme.colors.surface)
-
-                                )
-                                // end of TextField
-                            }
-
-                            LazyRow(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(start = 8.dp, bottom = 8.dp),
-                                state = scrollState
-                            ){
-                                scope.launch {
-                                    scrollState.scrollToItem(viewModel.itemIndex, viewModel.scrollOffset)
-                                }
-
-                                itemsIndexed(
-                                    items = getAllFoodCategory()
-                                ){ index, category ->
-                                   FoodCategoryChip(
-                                       category = category.value,
-                                       isSelected = selectedCategory == category,
-                                       onSelectedCategoryChanged = {
-                                         viewModel.onSelectedCategoryChanged(it)
-                                           viewModel.onChangeHorizontalScrollPosition(scrollState)
-                                       },
-                                       onExecuteSearch = viewModel :: newSearch
-                                   )
-                                }
-                            }
-                        }
-
-                    }
                     LazyColumn(
                         modifier = Modifier
                             .padding(start = 8.dp, end = 8.dp)
@@ -161,6 +102,11 @@ class RecipeListFragment: Fragment() {
 
             }
         }
+
+
+
     }
+
+
 
 }
